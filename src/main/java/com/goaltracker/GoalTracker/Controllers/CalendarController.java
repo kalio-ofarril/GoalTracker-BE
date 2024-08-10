1aa16f701712539ce3198e0bcb5c87d0b168b69c
Kalio <kalio.ofvil@gmail.com>
2024-08-09 19:25:49 -0600

    Starting app

--- src/main/java/com/goaltracker/GoalTracker/Controllers/CalendarController.java
+++ src/main/java/com/goaltracker/GoalTracker/Controllers/CalendarController.java
@@ -2,7 +2,6 @@ package com.goaltracker.GoalTracker.Controllers;
 
 import java.util.List;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +16,8 @@ public class CalendarController {
     private final CalendarService calendarService;
 
     @GetMapping("/calendar/getMonthData")
-    public ResponseEntity<List<String>> getMonthData() {
+    public List<String> getMonthData() {
 
-        return ResponseEntity.ok(calendarService.getMonthData());
+        return calendarService.getMonthData();
     }
 }
